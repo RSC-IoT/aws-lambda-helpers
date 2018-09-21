a0ac5f08494fd22edb65042099a6f4b65bee1247
John Veldboom <jveldboom@petsafe.net>
2018-09-21 16:24:46 -0400

    add tests to main code and change to use standard linting

--- index.js
+++ index.js
@@ -1,55 +1,58 @@
 const Response = {
 
-    success(body) {
-        return this.buildResponse(200,body)
-    },
+  success (body) {
+    return this.buildResponse(200, body)
+  },
 
-    resourceCreated(body) {
-        return this.buildResponse(201, body)
-    },
+  resourceCreated (body) {
+    return this.buildResponse(201, body)
+  },
 
-    error(body) {
-        return this.buildResponse(500,body)
-    },
+  error (body) {
+    return this.buildResponse(500, body)
+  },
 
-    notFound(body) {
-        return this.buildResponse(404,body)
-    },
+  notFound (body) {
+    return this.buildResponse(404, body)
+  },
 
-    badRequest(body) {
-        return this.buildResponse(400,body)
-    },
+  badRequest (body) {
+    return this.buildResponse(400, body)
+  },
 
-    alreadyExists(body) {
-        return this.buildResponse(409,body)
-    },
+  unauthorized (body) {
+    return this.buildResponse(401, body)
+  },
 
-    validationError(body) {
-        return this.buildResponse(422,body)
-    },
+  alreadyExists (body) {
+    return this.buildResponse(409, body)
+  },
 
-    buildResponse(statusCode,body) {
-        if (typeof body === 'object') {
-            body = JSON.stringify(body)
-        }
+  validationError (body) {
+    return this.buildResponse(422, body)
+  },
 
-        return {
-            statusCode,
-            body,
-        }
+  buildResponse (statusCode = 200, body) {
+    if (typeof body === 'object') {
+      body = JSON.stringify(body)
     }
+
+    return {
+      statusCode: parseInt(statusCode),
+      body
+    }
+  }
 }
 
 module.exports = {
-    Response,
-    Errors: {
-        Unauthorized: require('./module/errors/Unauthorized'),
-        UnprocessableEntity: require('./module/errors/UnprocessableEntity'),
-        InternalServerError: require('./module/errors/InternalServerError'),
-        Forbidden: require('./module/errors/Forbidden'),
-        NotFound: require('./module/errors/NotFound'),
-        Fail: require('./module/errors/Fail'),
-        AlreadyExists: require('./module/errors/AlreadyExists')
-    },
-
+  Response,
+  Errors: {
+    Unauthorized: require('./module/errors/Unauthorized'),
+    UnprocessableEntity: require('./module/errors/UnprocessableEntity'),
+    InternalServerError: require('./module/errors/InternalServerError'),
+    Forbidden: require('./module/errors/Forbidden'),
+    NotFound: require('./module/errors/NotFound'),
+    Fail: require('./module/errors/Fail'),
+    AlreadyExists: require('./module/errors/AlreadyExists')
+  }
 }
